5680bf37f0071122d96df266c19d041cacfc3fba
热爱可抵万难 <2426912619@qq.com>
2025-12-04 22:13:55 +0800

    mvc

---
+++ mvc/src/main/java/request/Request.java
@@ -0,0 +1,37 @@
+package request;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class Request {
+//    @RequestMapping(value = "/get",method = {RequestMethod.GET,RequestMethod.POST})
+//    @RequestMapping(value = "/get",method = RequestMethod.GET)
+    @GetMapping("/get")
+    public String get() {
+        System.out.println("get请求----------");
+        return "Vague";
+    }
+    //    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    @PostMapping("/post")
+    public String post() {
+        System.out.println("post请求----------");
+        return "Vague";
+    }
+    @RequestMapping(value = "/put", method = {RequestMethod.PUT, RequestMethod.POST})
+    public String put() {
+        System.out.println("put请求----------");
+        return "Vague";
+    }
+    @RequestMapping(value = "/delete", method = {RequestMethod.DELETE, RequestMethod.POST})
+    public String delete() {
+        System.out.println("delete请求----------");
+        return "Vague";
+    }
+    @RequestMapping(value = "/head", method = {RequestMethod.HEAD, RequestMethod.POST})
+    public String head() {
+        System.out.println("head请求----------");
+        return "Vague";
+    }
+
+}
