69ffc514ac7a7da417cee254de3fb71468d28a22
热爱可抵万难 <2426912619@qq.com>
2025-12-09 21:10:27 +0800

    mvc

--- mvc/src/main/java/yu/Application.java
+++ mvc/src/main/java/yu/Application.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Application {
-    @RequestMapping("/yu")
-    public String test() {
-        return "request";
-    }
+
     @RequestMapping("/httpApplication")
     public String session(HttpServletRequest request) {
         ServletContext application = request.getServletContext();
---
+++ mvc/src/main/java/yu/Config.java
@@ -0,0 +1,12 @@
+package yu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Config {
+    @RequestMapping("/yu")
+    public String test() {
+        return "request";
+    }
+}
