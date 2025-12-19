648d80cd4cadfe343e96f979f9cb86b8e780c4b2
热爱可抵万难 <2426912619@qq.com>
2025-12-19 23:01:04 +0800

    mvc

--- mvc/src/main/java/cookie/Templates.java
+++ mvc/src/main/java/cookie/Templates.java
@@ -6,15 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 
 public class Templates {
-    //一个请求对应一个方法
-    @RequestMapping("/first")
-    public String first() {
-        return "first";
-    }
-    @RequestMapping("/second")
-    public String second() {
-        return "second";
-    }
     //多个请求对应一个方法
 //    @RequestMapping(value = {"/one","/two"})
     @RequestMapping(path = {"/one","/two"})
