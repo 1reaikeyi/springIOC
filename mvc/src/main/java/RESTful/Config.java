e78f7c8c1a07356a07a3eb6bde7a285256e5d4c1
热爱可抵万难 <2426912619@qq.com>
2025-12-23 22:13:15 +0800

    mvc

--- mvc/src/main/java/RESTful/Config.java
+++ mvc/src/main/java/RESTful/Config.java
@@ -1,9 +1,10 @@
 package RESTful;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+@Controller
+public class Config {
     @RequestMapping("restful")
     public String test(){
         return "ok";
