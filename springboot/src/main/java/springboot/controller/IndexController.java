145e515b465e25987e71205fbc79350ed69c9398
热爱可抵万难 <2426912619@qq.com>
2026-01-02 19:05:59 +0800

    修改目录

---
+++ springboot/src/main/java/springboot/controller/IndexController.java
@@ -0,0 +1,18 @@
+package springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String index(){
+        return "springboot";
+    }
+    @RequestMapping("/chat")
+    public String chat(){
+        return "chat";
+    }
+}
