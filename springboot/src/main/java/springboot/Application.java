b0a6d21f513a898ae272a16c63c9ef3e9198c038
热爱可抵万难 <2426912619@qq.com>
2026-01-13 23:07:42 +0800

    spring boot

--- springboot/src/main/java/springboot/Application.java
+++ springboot/src/main/java/springboot/Application.java
@@ -11,12 +11,20 @@ public class Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext application = SpringApplication.run(Application.class, args);
         String[] beanNames = application.getBeanDefinitionNames();
-        int count = 0;
+//        int count = 0;
+//        for (String beanName : beanNames) {
+//            System.out.println(beanName);
+//            count++;
+//        }
+//        System.out.println("组件："+count);
         for (String beanName : beanNames) {
-            System.out.println(beanName);
-            count++;
+            if(beanName.equals("a")){
+                System.out.println("注册成功"+beanName);
+            }
+            if(beanName.equals("b")){
+                System.out.println("注册成功"+beanName);
+            }
         }
-        System.out.println(count);
 
         application.close();
 
