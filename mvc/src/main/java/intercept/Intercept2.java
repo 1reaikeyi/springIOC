58aaff9582d875bcb6cbb9ee6e2a619bbaac5b64
1reaikeyi <reaikeyi@qq.com>
2026-07-30 10:42:01 +0800

    feat(mvc): 事件发布与监听

---
+++ mvc/src/main/java/intercept/Intercept2.java
@@ -0,0 +1,26 @@
+package intercept;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class Intercept2 implements HandlerInterceptor{
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("启动preHandle：true");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("启动postHandle");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("启动afterCompletion");
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
