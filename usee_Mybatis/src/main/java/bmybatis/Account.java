487fd58aa66b36614a9dde1909bc4c7bf69e8c6a
1reaikeyi <reaikeyi@qq.com>
2026-07-30 10:41:45 +0800

    feat(mvc): 引入 Spring MVC 模块，搭建 Web 项目基础

---
+++ usee_Mybatis/src/main/java/bmybatis/Account.java
@@ -0,0 +1,37 @@
+package bmybatis;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = {"bmybatis"})
+@Component("ajbdc")
+public class Account {
+    private String accountant;
+    private int money;
+    public Account() {
+
+    }
+
+    public Account(String accountant, int money) {
+        this.accountant = accountant;
+        this.money = money;
+    }
+
+    public String getAccountant() {
+        return accountant;
+    }
+
+    public void setAccountant(String accountant) {
+        this.accountant = accountant;
+    }
+
+    public int getmoney() {
+        return money;
+    }
+
+    public void setmoney(int money) {
+        this.money = money;
+    }
+}
