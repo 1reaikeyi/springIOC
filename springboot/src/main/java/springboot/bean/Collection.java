e8f7e3038a35ef25a39c26607637f25a93ed300a
1reaikeyi <reaikeyi@qq.com>
2026-07-30 10:42:11 +0800

    feat(springboot): Web 模块 - 嵌入式 Tomcat

---
+++ springboot/src/main/java/springboot/bean/Collection.java
@@ -0,0 +1,59 @@
+package springboot.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties("collection")
+public class Collection {
+    private String[] names;
+    private User[] user;
+    private List<String> list;
+    private Map<String, String> map;
+
+    public String[] getNames() {
+        return names;
+    }
+
+    public void setNames(String[] names) {
+        this.names = names;
+    }
+
+    public User[] getUser() {
+        return user;
+    }
+
+    public void setUser(User[] user) {
+        this.user = user;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
+    public void show(){
+        System.out.println(Arrays.toString(names));
+        System.out.println(Arrays.toString(user));
+        System.out.println(list.toString());
+        System.out.println(map.toString());
+    }
+}
--- springboot/src/main/java/springboot/bean/ValueUser.java
+++ springboot/src/main/java/springboot/bean/ValueUser.java
@@ -1,4 +1,4 @@
-package springboot.controller;
+package springboot.bean;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
